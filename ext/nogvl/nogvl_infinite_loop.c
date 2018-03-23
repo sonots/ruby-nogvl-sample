b6a6803c81eb226d332dcca32c5fe790a5b8b782
sonots <sonots@gmail.com>
2018-03-23 22:47:45 +0900

    An example without global variable

--- ext/nogvl/nogvl_infinite_loop.c
+++ ext/nogvl/nogvl_infinite_loop.c
@@ -3,11 +3,10 @@
 
 #define N_LOOP 100000000000
 
-static int stop = 0;
-
 // rb_thread_call_without_gvl
 struct nogvl_infinite_loop_args {
     long n_loop;
+    int stop;
 };
 
 // rb_thread_call_without_gvl
@@ -15,24 +14,21 @@ static void* nogvl_infinite_loop(void *ptr)
 {
     struct nogvl_infinite_loop_args *args = ptr;
     printf("infinite_loop(%ld)\n", args->n_loop);
-    for (size_t i = 0; !stop && i < args->n_loop; i++);
+    for (size_t i = 0; !args->stop && i < args->n_loop; i++);
     return (void*)Qnil;
 }
 
 // rb_thread_call_without_gvl
-struct nogvl_infinite_loop_ubf_args {
-};
-
-// rb_thread_call_without_gvl
 static void nogvl_infinite_loop_ubf(void *ptr)
 {
-    stop = 1;
+    struct nogvl_infinite_loop_args *args = ptr;
+    args->stop = 1;
 }
 
 VALUE run_infinite_loop()
 {
     struct nogvl_infinite_loop_args args;
     args.n_loop = N_LOOP;
-    struct nogvl_infinite_loop_ubf_args ubf_args;
-    return (VALUE)rb_thread_call_without_gvl(nogvl_infinite_loop, &args, nogvl_infinite_loop_ubf, &ubf_args);
+    args.stop = 0;
+    return (VALUE)rb_thread_call_without_gvl(nogvl_infinite_loop, &args, nogvl_infinite_loop_ubf, &args);
 }
